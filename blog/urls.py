973e2a6f374cb4d9bf3f330b4456ff1f21779b33
Michelle Cordeiro <mic321@gmail.com>
2019-01-20 14:14:14 -0300

    05 Added view, template blog post, CSS, token

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('posts', views.PostModelViewSet)
 
 urlpatterns = [
-	path('',views.post_list, name='post_list'),
+	path('', views.post_list, name='post_list'),
+	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	path('api/v1/', include(router.urls)),
+	path('api/v1/token/', views.CreateTokenView.as_view(), name='token')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
+from .models import Post
+from .serializers import PostModelSerializer
+
+from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from .serializers import PostModelSerializer, AuthTokenSerializer
 from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-	return render(request, 'blog/post_list.html', {'posts': posts})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostModelViewSet(viewsets.ModelViewSet):
+	serializer_class = PostModelSerializer
+	queryset = Post.objects.all().order_by('-title')
+
+class CreateTokenView(ObtainAuthToken):
+	serializer_class = AuthTokenSerializer
+	renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
